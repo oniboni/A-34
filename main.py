95bc85fa034bfca683e54a93bc9185b9de8e1e43
Oni Home <onibonioni@gmail.com>
2015-12-16 22:09:04 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+__author__ = 'oni'
+
+from segment import Segment
+from logicStuff import LogicNet
+
+def main():
+    input = "01 b0 c4 81 a6 c5 81 a0 5f 01 b7 5f"
+
+    logic = LogicNet(int('b',16))
+    for char in input.replace(' ', ''):
+        logic.readInput(int(char,16))
+        segment = Segment(logic.followPaths())
+        print segment.printSegment()
+
+
+if __name__ == '__main__':
+    main()
---
+++ segment.py
@@ -0,0 +1,62 @@
+__author__ = 'oni'
+
+
+class Segment:
+
+    def __init__(self, segments):
+        self.A = segments[0]
+        self.B = segments[1]
+        self.C = segments[2]
+        self.D = segments[3]
+        self.E = segments[4]
+        self.F = segments[5]
+        self.G = segments[6]
+        self.p = segments[7]
+
+    def printSegment(self):
+
+        result = ['000', '000', '000', '000', '000']
+
+        if self.A:
+            result[0] = '020'
+        if self.B:
+            result[1] = '001'
+        if self.C:
+            result[3] = '001'
+        if self.D:
+            result[4] = '020'
+        if self.E:
+            if result[3] == '001':
+                result[3] = '101'
+            else:
+                result[3] = '100'
+        if self.F:
+            if result[1] == '001':
+                result[1] = '101'
+            else:
+                result[1] = '100'
+        if self.G:
+            result[2] = '020'
+
+        self.render( self.scale (result,3) )
+
+    # copy&paste from https://stackoverflow.com/questions/18427782/seven-segment-display-with-width
+    def scale(self, code, factor):
+        if factor == 1:
+            return code
+
+        result = ["{}{}{}".format(line[0], line[1:-1]*factor, line[-1])  # widen
+                  for line in code]
+
+        for i in range(len(result)-2, 0, -2):
+            result[i:i+1] = result[i:i+1]*factor       # stretch vertically
+
+        return result
+
+    def render(self, code):
+
+        if self.p:
+            code[len(code)-1] += '.'
+
+        for line in code:
+            print(line.replace('0', ' ').replace('1', '|').replace('2', '-'))
