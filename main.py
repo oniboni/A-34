751dd36911241093202dee01d1095e98d2e1c2c0
Oni Home <onibonioni@gmail.com>
2015-12-18 16:49:08 +0100

    string output added

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from logic.logicNet import LogicNet
 def main():
 
     # cheap hex counter simulation :)
-    test_input = "01 23 45 67 89 ab cd ef"
+    test_input = "01 23 45 67 89 ab cd ef"   # "01 b0 c4 81 a6 c5 81 a0 5f 01 b7 5f"
 
     logic = LogicNet(int('b', 16))
     for char in test_input.replace(' ', ''):
         logic.read_input(int(char, 16))
         segment = Segment(logic.follow_paths())
-        print segment.print_segment()
+        # print segment.print_segment()
+
+    segment.print_chars()
 
 
 if __name__ == '__main__':
--- segment.py
+++ segment.py
@@ -2,6 +2,7 @@ __author__ = 'oni'
 
 
 class Segment:
+    segment_history = []
 
     def __init__(self, segments):
         self.A = segments[0]
@@ -12,6 +13,42 @@ class Segment:
         self.F = segments[5]
         self.G = segments[6]
         self.p = segments[7]
+
+        self.segment_history.append([self.A, self.B, self.C, self.D, self.E, self.F, self.G, self.p])
+
+    def print_chars(self):
+
+        out = self.build_output_string(self.segment_history, '')
+        print out
+
+    def build_output_string(self, history, output_string):
+
+        if len(history) == 0:
+            return output_string
+
+        if history[0][:7] == [True, True, True, False, True, True, True]:
+            output_string += 'A'
+        if history[0][:7] == [True, False, True, True, False, True, True]:
+            output_string += 'S'
+        if history[0][:7] == [False, False, False, True, True, True, True]:
+            output_string += 't'
+        if history[0][:7] == [True, False, False, True, True, True, True]:
+            output_string += 'E'
+        if history[0][:7] == [False, False, False, False, True, False, True]:
+            output_string += 'r'
+        if history[0][:7] == [False, True, True, False, False, False, False]:
+            output_string += '1'
+        if history[0][:7] == [False, False, True, True, True, False, True]:
+            output_string += 'o'
+        if history[0][:7] == [False, False, True, False, True, False, True]:
+            output_string += 'n'
+        if history[0][:7] == [True, True, True, False, False, True, True]:
+            output_string += '9'
+
+        if history[0][7]:
+            output_string += '.'
+
+        return self.build_output_string(history[1:], output_string)
 
     def print_segment(self):
 
@@ -40,7 +77,7 @@ class Segment:
         # if self.p:
         #     result[4][2] = '.'
 
-        self.render(self.scale(result, 3))
+        return self.render(self.scale(result, 3))
 
     # copy&paste from https://stackoverflow.com/questions/18427782/seven-segment-display-with-width
     @staticmethod
